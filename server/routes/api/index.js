a472be3bf6455d6fbdb5e7865ee9c13fc9600276
jacob244453 <85600569+jacob244453@users.noreply.github.com>
2021-11-14 17:02:58 -0500

    Update index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+// const router = require('express').Router();
 
-router.use('/users', userRoutes);
+// const userRoutes = require('./user-routes');
 
-module.exports = router;
+// router.use('/users', userRoutes);
+
+// module.exports = router;
