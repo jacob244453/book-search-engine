5186e7ae14740554ba8a573bf36371ff61431e3f
jacob244453 <85600569+jacob244453@users.noreply.github.com>
2021-11-14 16:39:35 -0500

    refactor

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -26,7 +26,7 @@ const SearchBooks = () => {
   const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  // learn more here:  https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,4 @@
 import gql from "graphql-tag";
-
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
